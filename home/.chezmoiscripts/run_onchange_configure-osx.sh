815ae04beecd6c2497d87ca9df0148c785b487cf
Atsuya Yamagami <my2502@icloud.com>
2025-12-02 01:15:35 +0900

    chore: F キーの設定を追加

--- home/.chezmoiscripts/run_onchange_configure-osx.sh
+++ home/.chezmoiscripts/run_onchange_configure-osx.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # NOTE: https://qiita.com/keitean/items/6972b8f34636a88bae74
 
+# F1, F2, ... を標準のファンクションキーとして使用する
+defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
+
 # デスクトップのアイコンをグリッドに沿うようにする
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
